cdbfc910b3d41ae1b7b83d7623ae54c705057580
MauroFelipe <mauro.felipe.oc@gmail.com>
2023-09-23 22:52:22 -0300

    added hateoas

---
+++ 14_HATEOAS/src/test/java/com/example/unittests/mockito/services/PersonServicesTest.java
@@ -0,0 +1,81 @@
+package com.example.unittests.mockito.services;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import com.example.model.Person;
+import com.example.repositories.PersonRepository;
+import com.example.services.PersonServices;
+import com.example.unittests.mapper.mocks.MockPerson;
+
+@TestInstance(Lifecycle.PER_CLASS)
+@ExtendWith(MockitoExtension.class)
+class PersonServicesTest {
+	
+	MockPerson input;
+	
+	@InjectMocks
+	private PersonServices service;
+	
+	@Mock
+	PersonRepository repository;
+
+	@BeforeEach
+	void setUp() throws Exception {
+		input = new MockPerson();
+		MockitoAnnotations.openMocks(this);
+	}
+
+	@Test @Disabled
+	void testFindAll() {		
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testFindById() {
+		Person entity = input.mockEntity(1); 
+		entity.setId(1L);
+		
+		when(repository.findById(1L)).thenReturn(Optional.of(entity));
+		
+		var result = service.findById(1L);
+		assertNotNull(result);
+		assertNotNull(result.getKey());
+		assertNotNull(result.getLinks());
+		
+		assertTrue(result.toString().contains("links: [</api/person/v1/1>;rel=\"self\"]"));
+		assertEquals("Addres Test1", result.getAddress());
+		assertEquals("First Name Test1", result.getFirstName());
+		assertEquals("Last Name Test1", result.getLastName());
+		assertEquals("Female", result.getGender());
+	}
+
+	@Test @Disabled
+	void testCreate() {
+		fail("Not yet implemented");
+	}
+
+	@Test @Disabled
+	void testUpdate() {
+		fail("Not yet implemented");
+	}
+
+	@Test @Disabled
+	void testDelete() {
+		fail("Not yet implemented");
+	}
+
+}
