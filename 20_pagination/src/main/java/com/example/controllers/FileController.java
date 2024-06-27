b3e17a958d0304672929aecef2b1a22de6201d9b
MauroFelipe <mauro.felipe.oc@gmail.com>
2024-06-27 08:02:19 -0300

    2105 Criando o endpoint para Upload

---
+++ 20_pagination/src/main/java/com/example/controllers/FileController.java
@@ -0,0 +1,41 @@
+package com.example.controllers;
+
+import java.util.logging.Logger;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import com.example.data.vo.v1.UploadFileResponseVO;
+import com.example.services.FileStorageService;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+
+@Tag(name = "File endpoint")
+@RestController
+@RequestMapping("/api/file/v1")
+public class FileController {
+	
+	private Logger logger = Logger.getLogger(FileController.class.getName());
+	
+	@Autowired
+	private FileStorageService fileStorageService;
+	
+	@PostMapping("/uploadFile")
+	public UploadFileResponseVO uploadFile(@RequestParam("file") MultipartFile file) {
+		logger.info("Storing file to disk");
+		
+		var filename = fileStorageService.storeFile(file);
+		String fileDownloadUri = ServletUriComponentsBuilder.fromCurrentContextPath()
+				.path("/api/file/v1/downloadFile/")
+				.path(filename)
+				.toUriString();
+		
+		return new UploadFileResponseVO(filename, fileDownloadUri, file.getContentType(), file.getSize());
+	}
+
+}
